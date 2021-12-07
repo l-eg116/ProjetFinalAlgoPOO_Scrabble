ed839a6aae8192f93ded89491201128db9e582da
l_eg <legcom6@gmail.com>
2021-12-07 11:32:27 +0100

    Add project files.

---
+++ ProjetFinalAlgoPOO_Scrabble/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetFinalAlgoPOO_Scrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
