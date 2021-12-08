2cd050ab0bd318bd9ac249285bf96423ae2202db
l_eg <legcom6@gmail.com>
2021-12-08 10:58:43 +0100

    Ajout des constructeurs pour Dictionnaire

--- ProjetFinalAlgoPOO_Scrabble/Dictionnaire.cs
+++ ProjetFinalAlgoPOO_Scrabble/Dictionnaire.cs
@@ -1,10 +1,61 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace ProjetFinalAlgoPOO_Scrabble
 {
     class Dictionnaire
     {
+        private List<string> contenu = new List<string> { };
+        private string langue = "unspecified";
+
+        public List<string> Contenu
+        {
+            get { return this.contenu; }
+        }
+        public int Taille
+        {
+            get { return this.contenu.Count; }
+        }
+        public string Langue
+        {
+            get { return this.langue; }
+        }
+
+        public Dictionnaire(string path = "Default_Dictionnaire.txt")
+        {
+            using(TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { " ", ",", ";" });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                while(!csvParser.EndOfData)
+                {
+                    string[] fields = csvParser.ReadFields();
+
+                    if(fields.Length == 2 && fields[0].ToLower() == "langue")
+                    {
+                        this.langue = fields[1];
+                        continue;
+                    }
+
+                    foreach(string mot in fields)
+                        if(mot.All(char.IsLetter))
+                            this.contenu.Add(mot.ToUpper());
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string return_str = "";
+
+            foreach(string mot in this.contenu)
+                return_str += mot + " ";
+
+            return return_str;
+        }
     }
 }
--- ProjetFinalAlgoPOO_Scrabble/Program.cs
+++ ProjetFinalAlgoPOO_Scrabble/Program.cs
@@ -6,12 +6,10 @@ namespace ProjetFinalAlgoPOO_Scrabble
     {
         static void Main(string[] args)
         {
-            SacJetons sac = new SacJetons(@"C:\Users\legco\Downloads\Jetons.txt");
-            Console.WriteLine(sac.ToString());
-
-            Jeton jeton;
-            while((jeton = sac.Piocher()) != null)
-                Console.WriteLine(jeton.ToString());
+            Dictionnaire fr = new Dictionnaire();
+            
+            Console.WriteLine(fr.Taille);
+            Console.WriteLine(fr.Langue);
         }
     }
 }
