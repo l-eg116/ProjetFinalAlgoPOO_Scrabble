bdd1f9670863cac53afe371e7a4c0af67cf8720b
33782 <33782@DESKTOP-CQT8GU7>
2021-12-09 08:48:59 +0100

    constructeur et méthode pour joueur

--- ProjetFinalAlgoPOO_Scrabble/Joueur.cs
+++ ProjetFinalAlgoPOO_Scrabble/Joueur.cs
@@ -6,5 +6,45 @@ namespace ProjetFinalAlgoPOO_Scrabble
 {
     class Joueur
     {
+        string nom;
+        int score = 0;
+        string[] mot_trouves = null;
+
+        public Joueur(string nom, int score, string[] mot_trouves)
+        {
+            if(nom != null)
+            {
+                this.nom = nom;
+            }
+            this.score = score;
+            this.mot_trouves = mot_trouves;
+        }
+        public void Add_Mot(string mot)
+        {
+            string[] tableau_remplacement = new string[mot_trouves.Length + 1];
+            for(int i = 0; i < mot_trouves.Length; i++)
+            {
+                tableau_remplacement[i] = mot_trouves[i];
+            }
+            mot_trouves = null;
+            mot_trouves = new string[tableau_remplacement.Length];
+            mot_trouves = tableau_remplacement;
+        }
+        public void Add_Score(int val)
+        {
+            score += val;
+        }
+        public override string ToString()
+        {
+            string phrase= "Le joueur s'appelle " + nom + ", il a " + score + " points \n";
+            if(mot_trouves != null)
+            {
+                for(int i = 0; i < mot_trouves.Length; i++)
+                {
+                    phrase += mot_trouves[i] + "\n";
+                }
+            }
+            return phrase;
+        }
     }
 }
