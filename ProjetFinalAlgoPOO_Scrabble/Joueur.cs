f7efcda4d910248b88bc019eb299bddb02d4a2ff
l_eg <legcom6@gmail.com>
2021-12-11 23:18:12 +0100

    Maintenance sur la classe Joueur

--- ProjetFinalAlgoPOO_Scrabble/Joueur.cs
+++ ProjetFinalAlgoPOO_Scrabble/Joueur.cs
@@ -1,8 +1,6 @@
-﻿using System.IO;
+﻿using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualBasic.FileIO;
 
 namespace ProjetFinalAlgoPOO_Scrabble
 {
@@ -14,26 +12,11 @@ namespace ProjetFinalAlgoPOO_Scrabble
         private List<Jeton> main_courante = new List<Jeton> { };
 
         /// <summary>
-        /// Constructeur de la class Joueur, si le nom est null alors la classe n'est pas faisable.
-        /// </summary>
-        /// <param name="nom">Nom du joueur</param>
-        /// <param name="score">Score du joueur</param>
-        /// <param name="mot_trouves">Mots trouvés par le joueur</param>
-        public Joueur(string nom, int score, List<string> mot_trouves)
-        {
-            if (nom != null)
-            {
-                this.nom = nom;
-            }
-            this.score = score;
-            this.mot_trouves = mot_trouves;
-        }
-        /// <summary>
         /// Nom du joueur
         /// </summary>
         public string Nom
         {
-            get{ return this.nom; }
+            get { return this.nom; }
         }
         /// <summary>
         /// Score du joueur
@@ -41,48 +24,72 @@ namespace ProjetFinalAlgoPOO_Scrabble
         public int Score
         {
             get { return this.score; }
+            set { this.score = value; }
         }
         /// <summary>
         /// Mots trouvés par le joueur
         /// </summary>
-        public List<string> Mot_Trouves
+        public List<string> MotTrouves
         {
             get { return this.mot_trouves; }
         }
         /// <summary>
         /// Jetons du joueurs
         /// </summary>
-        public List<Jeton> Main_Courante
+        public List<Jeton> MainCourante
         {
             get { return this.main_courante; }
         }
+
         /// <summary>
-        /// Creer un nouveau joueur à partir de Joueurs.txt
+        /// Constructeur de la class Joueur
+        /// </summary>
+        /// <param name="nom">Nom du joueur</param>
+        /// <param name="score">Score du joueur</param>
+        /// <param name="mot_trouves">Mots trouvés par le joueur</param>
+        /// <param name="main_courante">Jetons tenus par le joueur</param>
+        public Joueur(string nom, int score = 0, List<string> mot_trouves = null, List<Jeton> main_courante = null)
+        {
+            this.nom = nom;
+            this.score = score;
+            if(mot_trouves != null)
+                this.mot_trouves = mot_trouves;
+            if(main_courante != null)
+                this.main_courante = main_courante;
+        }
+        /// <summary>
+        /// Creer un nouveau joueur à partir d'un fichier
+        /// "Nom";Score
+        /// mot1;mot2;mot3;...
+        /// jeton1;jeton2;jeton3;...
         /// </summary>
         /// <param name="path">Chemin du fichier</param>
-        public Joueur(string path = "Joueurs.csv")
+        public Joueur(string path)
         {
-            using (TextFieldParser csvParser = new TextFieldParser(path))
+            using(TextFieldParser csvParser = new TextFieldParser(path))
             {
                 csvParser.CommentTokens = new string[] { "#" };
                 csvParser.SetDelimiters(new string[] { ",", ";" });
                 csvParser.HasFieldsEnclosedInQuotes = true;
+
                 string[] fields = csvParser.ReadFields();
-                if (!csvParser.EndOfData)
-                {
-                    this.nom = fields[0];
-                    this.score = Convert.ToInt32(fields[1]);
-                    fields = csvParser.ReadFields();
-                    for(int i = 0; i < fields.Length; i++)
-                    {
-                        Add_Mot(fields[i]);
-                    }
-                    fields = csvParser.ReadFields();
-                    for(int i = 0; i < fields.Length; i++)
-                    {
-                        this.main_courante.Add(new Jeton(Convert.ToChar(fields[i])));
-                    }
-                }
+                this.nom = fields[0];
+                try { this.score = Convert.ToInt32(fields[1]); }
+                catch(System.FormatException) { this.score = 0; }
+                catch(System.IndexOutOfRangeException) { this.score = 0; }
+
+                fields = csvParser.ReadFields();
+                if(fields != null)
+                    foreach(string mot in fields)
+                        this.mot_trouves.Add(Dictionnaire.RemoveDiacritics(mot.ToUpper()));
+
+                fields = csvParser.ReadFields();
+                if(fields != null)
+                    foreach(string jeton in fields)
+                        try
+                        { this.main_courante.Add(new Jeton(Convert.ToChar(jeton))); }
+                        catch(System.FormatException) { }
+
             }
         }
 
@@ -90,35 +97,46 @@ namespace ProjetFinalAlgoPOO_Scrabble
         /// Permet de rajouter un mot dans les mots trouvés
         /// </summary>
         /// <param name="mot">Mot à rajouter dans les mots trouvés</param>
+        public void TrouveMot(string mot)
+        {
+            this.mot_trouves.Add(Dictionnaire.RemoveDiacritics(mot.ToUpper()));
+        }
+        /// <summary>
+        /// Nom demandé dans la consigne
+        /// </summary>
+        /// <param name="mot">Mot que le joueur à trouvé</param>
         public void Add_Mot(string mot)
         {
-            this.mot_trouves.Add(mot);
+            this.TrouveMot(mot);
         }
 
         /// <summary>
+        /// [DEPRECATED]
         /// Permet de modifier le score du joueur
+        /// UTILISER LE 'set' DE L'ATTRIBUT DE CLASSE Joueur.Score
         /// </summary>
         /// <param name="val">Valeur à rajouter au score du joueur</param>
         public void Add_Score(int val)
         {
             score += val;
         }
+
         public override string ToString()
         {
             string phrase= "Le joueur s'appelle " + nom + ", il a " + score + " points \n";
             if(mot_trouves != null && mot_trouves.Count > 0)
             {
                 phrase += "Le joueur a trouvé le(s) mot(s) : ";
-                for (int i = 0; i < mot_trouves.Count - 1; i++)
+                for(int i = 0; i < mot_trouves.Count - 1; i++)
                 {
                     phrase += mot_trouves[i] + "; ";
                 }
                 phrase += mot_trouves[mot_trouves.Count - 1] + "\n";
             }
-            if(main_courante != null && main_courante.Count>0)
+            if(main_courante != null && main_courante.Count > 0)
             {
                 phrase += "Le joueur a le(s) jeton(s) suivant dans sa main : ";
-                for (int i = 0; i < main_courante.Count - 1; i++)
+                for(int i = 0; i < main_courante.Count - 1; i++)
                 {
                     phrase += main_courante[i] + "; ";
                 }
@@ -132,18 +150,41 @@ namespace ProjetFinalAlgoPOO_Scrabble
         /// Permet de rajouter un jeton dans la main courante du joueur
         /// </summary>
         /// <param name="monjeton">Jeton à rajouter dans la main courante du joueur</param>
+        public void DonnerJeton(Jeton monjeton)
+        {
+            this.main_courante.Add(monjeton);
+        }
+        /// <summary>
+        /// Nom demandé par la consigne
+        /// </summary>
+        /// <param name="monjeton"></param>
         public void Add_Main_Courante(Jeton monjeton)
         {
-            this.main_courante.Add(new Jeton(Convert.ToChar(monjeton)));
+            this.DonnerJeton(monjeton);
         }
 
         /// <summary>
         /// Permet d'enlever un jeton dans la main courante du joueur
         /// </summary>
         /// <param name="monjeton">Jeton à enlever dans la main courante du joueur</param>
-        public void Remove_Main_Courante(Jeton monjeton)
+        public bool EnleverJeton(Jeton monjeton)
         {
-            this.main_courante.Remove(new Jeton(Convert.ToChar(monjeton)));
+            for(int i = 0; i < this.main_courante.Count; i++)
+                if(this.main_courante[i] == monjeton)
+                {
+                    this.main_courante.RemoveAt(i);
+                    return true;
+                }
+            return false;
+        }
+        /// <summary>
+        /// Nom demandé par la consigne
+        /// </summary>
+        /// <param name="monjeton"></param>
+        /// <returns></returns>
+        public bool Remove_Main_Courante(Jeton monjeton)
+        {
+            return this.EnleverJeton(monjeton);
         }
     }
 }
--- ProjetFinalAlgoPOO_Scrabble/Program.cs
+++ ProjetFinalAlgoPOO_Scrabble/Program.cs
@@ -7,9 +7,22 @@ namespace ProjetFinalAlgoPOO_Scrabble
     {
         static void Main(string[] args)
         {
-            Plateau plat = new Plateau(@"C:\Users\legco\Downloads\InstancePlateau.txt");
+            Joueur michel = new Joueur(nom: "Michel");
+            Console.WriteLine(michel.ToString());
 
-            Console.WriteLine(plat.ToString());
+            Joueur patrick = new Joueur("Patrick", 15, new List<string> {"BONJOUR"}, new List<Jeton> {new Jeton('y')});
+            Console.WriteLine(patrick.ToString());
+            
+            Joueur benjamin = new Joueur(path: @"C:\Users\legco\Source\Repos\l-eg116\ProjetFinalAlgoPOO_Scrabble\ProjetFinalAlgoPOO_Scrabble\Joueurs.txt");
+            Console.WriteLine(benjamin.ToString());
+
+            benjamin.Score += 5;
+            benjamin.TrouveMot("mandibule");
+            benjamin.DonnerJeton(new Jeton('y'));
+            benjamin.DonnerJeton(new Jeton('k'));
+            benjamin.EnleverJeton(new Jeton('e'));
+
+            Console.WriteLine(benjamin.ToString());
         }
     }
 }
