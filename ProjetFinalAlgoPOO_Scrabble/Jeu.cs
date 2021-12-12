fabb4ae012fd972342528bef775070456291c244
33782 <33782@DESKTOP-CQT8GU7>
2021-12-13 00:18:14 +0100

    pratiquement rien :((

---
+++ ProjetFinalAlgoPOO_Scrabble/Jeu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetFinalAlgoPOO_Scrabble
+{
+    class Jeu
+    {
+        Dictionnaire[] mondico;
+        Plateau monplateau;
+        SacJetons monsac_jetons;
+        /// <summary>
+        /// Constructeur de la classe Jeu
+        /// </summary>
+        /// <param name="mondico"></param>
+        /// <param name="monplateau"></param>
+        /// <param name="monsac_jetons"></param>
+        public Jeu(Dictionnaire[] mondico, Plateau monplateau, SacJetons monsac_jetons)
+        {
+            mondico = new Dictionnaire("Francais.txt")
+            this.mondico = mondico;
+            this.monplateau = monplateau;
+            this.monsac_jetons = monsac_jetons;
+        }
+       
+    }
+}
