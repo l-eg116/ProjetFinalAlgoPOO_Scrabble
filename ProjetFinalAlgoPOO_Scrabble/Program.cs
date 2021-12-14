1a8a6cb443aa97dd0f1298bdc17bd4f63bd60a55
33782 <33782@DESKTOP-CQT8GU7>
2021-12-14 17:30:23 +0100

    légende plateau

--- ProjetFinalAlgoPOO_Scrabble/Program.cs
+++ ProjetFinalAlgoPOO_Scrabble/Program.cs
@@ -9,7 +9,36 @@ namespace ProjetFinalAlgoPOO_Scrabble
         {
             Plateau plat = new Plateau(@"C:\Users\33782\Source\Repos\ProjetFinalAlgoPOO_Scrabble\ProjetFinalAlgoPOO_Scrabble\Test_Plateau2.txt");
             plat.Afficher();
-            Dictionnaire dict = new Dictionnaire();
+            
+            Console.WriteLine("\nAvec : \n");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" et ");
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : MOT COMPTE DOUBLE ");
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : MOT COMPTE TRIPLE");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : LETTRE COMPTE DOUBLE");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : LETTRE COMPTE TRIPLE");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : AUCUN BONUS");
         }
     }
 }
