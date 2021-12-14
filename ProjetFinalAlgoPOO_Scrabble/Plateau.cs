f6c46b091def8d680fe395944d55e1f285f44879
l_eg <legcom6@gmail.com>
2021-12-14 18:13:06 +0100

    Déplacement de la la méthode Légende

--- ProjetFinalAlgoPOO_Scrabble/Plateau.cs
+++ ProjetFinalAlgoPOO_Scrabble/Plateau.cs
@@ -208,7 +208,38 @@ namespace ProjetFinalAlgoPOO_Scrabble
             }
             Console.ResetColor();
         }
+        public static void AfficherLegende()
+        {
+            Console.WriteLine("\nAvec : \n");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" et ");
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : MOT COMPTE DOUBLE ");
 
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : MOT COMPTE TRIPLE");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : LETTRE COMPTE DOUBLE");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : LETTRE COMPTE TRIPLE");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" : AUCUN BONUS");
+        }
         public bool TesterMot(string mot, int x, int y, char rot, Dictionnaire dico, List<string> out_mots_crees = null)
         {
             mot = Dictionnaire.RemoveDiacritics(mot.ToUpper());
--- ProjetFinalAlgoPOO_Scrabble/Program.cs
+++ ProjetFinalAlgoPOO_Scrabble/Program.cs
@@ -7,41 +7,9 @@ namespace ProjetFinalAlgoPOO_Scrabble
     {
         static void Main(string[] args)
         {
-            Plateau plat = new Plateau(@"C:\Users\33782\Source\Repos\ProjetFinalAlgoPOO_Scrabble\ProjetFinalAlgoPOO_Scrabble\Test_Plateau2.txt");
+            Plateau plat = new Plateau();
             plat.Afficher();
-            Legende();
-        }
-        static void Legende()
-        {
-            Console.WriteLine("\nAvec : \n");
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write(" et ");
-            Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(" : MOT COMPTE DOUBLE ");
-
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(" : MOT COMPTE TRIPLE");
-
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(" : LETTRE COMPTE DOUBLE");
-
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(" : LETTRE COMPTE TRIPLE");
-
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.Write("  ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(" : AUCUN BONUS");
+            Plateau.AfficherLegende();
         }
     }
 }
