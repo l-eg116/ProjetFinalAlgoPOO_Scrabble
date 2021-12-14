291a7511e22ca27a0562652a4daeadba7381a7bf
l_eg <legcom6@gmail.com>
2021-12-14 18:44:40 +0100

    Update Jeu.cs

--- ProjetFinalAlgoPOO_Scrabble/Jeu.cs
+++ ProjetFinalAlgoPOO_Scrabble/Jeu.cs
@@ -6,27 +6,9 @@ namespace ProjetFinalAlgoPOO_Scrabble
 {
     class Jeu
     {
-        private Plateau plateau;
-        private List<Joueur> joueurs = new List<Joueur> { };
-        private SacJetons sac_jetons;
-        private Dictionnaire dictionnaire;
-
-        /// <summary>
-        /// Constructeur par défaut de Jeu
-        /// </summary>
-        /// <param name="plateau">Instance de plateau</param>
-        /// <param name="sac_jetons">Instance de SacJetons</param>
-        /// <param name="dictionnaire">Instance de Dictionnaire</param>
-        /// <param name="joueurs">Liste de Joueur</param>
-        public Jeu(Plateau plateau, SacJetons sac_jetons, Dictionnaire dictionnaire = null, List<Joueur> joueurs = null)
+        static void Main()
         {
-            this.plateau = plateau;
-            this.sac_jetons = sac_jetons;
-            if(dictionnaire == null)
-                this.dictionnaire = new Dictionnaire();
-            if(joueurs == null)
-                this.joueurs = new List<Joueur> { };
+
         }
-       
     }
 }
