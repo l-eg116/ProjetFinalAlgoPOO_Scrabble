ad33a00852c3901b7e558e5a2948e0ef53340762
l_eg <legcom6@gmail.com>
2021-12-15 13:12:37 +0100

    Fixes

--- ProjetFinalAlgoPOO_Scrabble/Jeu.cs
+++ ProjetFinalAlgoPOO_Scrabble/Jeu.cs
@@ -5,22 +5,22 @@ namespace ProjetFinalAlgoPOO_Scrabble
 {
     class Jeu
     {
-        static private Plateau plateau;
-        static public List<Joueur> joueurs = new List<Joueur> { };
-        static private SacJetons sac_jetons;
-        static private Dictionnaire dictionnaire;
+        static Plateau plateau;
+        static List<Joueur> joueurs = new List<Joueur> { };
+        static SacJetons sac_jetons;
+        static Dictionnaire dictionnaire;
 
 
         public static void Main_()
         {
-            Plateau plat = new Plateau();
-            plat.Afficher();
+            plateau = new Plateau();
+            plateau.Afficher();
             Plateau.AfficherLegende();
             Console.WriteLine("\nCombien de joueurs vont jouer (2 à 4 joueurs) ?");
             int nombrejoueurs = Convert.ToInt32(Console.ReadLine());
             for(int i = 1; i <= nombrejoueurs; i++)
             {
-                if (i == 1)
+                if(i == 1)
                 {
                     Console.WriteLine("Nom du " + i + "er joueur : ");
                 }
@@ -30,9 +30,9 @@ namespace ProjetFinalAlgoPOO_Scrabble
                 }
                 string nomjoueur = Console.ReadLine();
                 joueurs.Add(new Joueur(nom: nomjoueur, score: 0));
-                
-                }
-            plat.Afficher();
+
+            }
+            plateau.Afficher();
             Plateau.AfficherLegende();
 
         }
