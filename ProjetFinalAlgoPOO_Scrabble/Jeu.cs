6671dd78cd8b9a0252ed03cfab92868cd022bde1
l_eg <legcom6@gmail.com>
2021-12-15 12:07:09 +0100

    Attributs pour Jeu

--- ProjetFinalAlgoPOO_Scrabble/Jeu.cs
+++ ProjetFinalAlgoPOO_Scrabble/Jeu.cs
@@ -6,6 +6,11 @@ namespace ProjetFinalAlgoPOO_Scrabble
 {
     class Jeu
     {
+        private Plateau plateau;
+        private List<Joueur> joueurs = new List<Joueur> { };
+        private SacJetons sac_jetons;
+        private Dictionnaire dictionnaire;
+
         static void Main()
         {
 
